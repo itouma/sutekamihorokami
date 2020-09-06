1b4d2798958753c6f6c3b9b9e3c33377a3dc57d4
itouma <68621609+itouma@users.noreply.github.com>
2020-09-06 15:12:30 +0900

    gem導入deviseモデル2つ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :exhibitions
+  devise_for :customers
   get 'items/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'items#index'
