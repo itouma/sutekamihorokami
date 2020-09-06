cb0adfa6a22a5736d8a883609e5208d9ffc0e91f
itouma <68621609+itouma@users.noreply.github.com>
2020-09-06 17:35:09 +0900

    コントローラー仮作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :exhibitions
-  devise_for :customers
+  devise_for :exhibitions, controllers: {sessions: 'exhibitions/sessions', passwords: 'exhibitions/passwords', registrations: 'exhibitions/registrations' }
+
+  devise_for :customers, controllers: {sessions: 'customers/sessions', passwords: 'customers/passwords', registrations: 'customers/registrations' }
+  
   get 'items/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'items#index'
