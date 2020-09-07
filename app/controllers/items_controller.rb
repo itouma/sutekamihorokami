a46aa4b50b2b9387385d68b92a5d4b2a0bbf547c
itouma <68621609+itouma@users.noreply.github.com>
2020-09-07 13:06:06 +0900

    db:seeds作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   skip_before_action :authenticate_customer!, only: [:index, :show]
-  before_action :move_to_index, except: [:index, :show]
-  #customer!もindexとshowと購入しかできないようにしたいから制限したい
+  # before_action :move_to_index, except: [:index, :show]
+  #customer!もindexとshowと購入しかできないようにしたいから制限したいあっちで設定
 
   def index
   end
@@ -10,10 +10,10 @@ class ItemsController < ApplicationController
 
   private
 
-  def move_to_index
-    if customer_signed_in? && @customer.id == current_customer.id 
-      redirect_to action: :index
-    end
-  end
+  # def move_to_index
+  #   if customer_signed_in? && @customer.id == current_customer.id 
+  #     redirect_to action: :index
+  #   end
+  # end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Exhibition.create!(
+  email: '2@2',
+  company_name: '株式会社テスト2',
+  password: '222222q',
+  encrypted_password: '222222q',
+  company_name_kana: 'カブシキガイシャテスト',
+  postal_code: '222-2222',
+  phone_number: '22222222222',
+  prefecture_id: '23',
+  city: '名古屋',
+  city_number: 'ポッカサッポロ',
+  building_name: 'サントリー'
+)
+
+  Customer.create!(
+    email: 's@s',
+    nickname: 'テスト太郎',
+    password: 'ssssss1',
+    encrypted_password: 'ssssss1',
+    family_name: 'クァ',
+    first_name: 'クァ',
+    family_name_kana: 'クァ',
+    first_name_kana: 'クァ',
+    birthday: '1988-12-25',
+    postal_code: '111-1111',
+    phone_number: '11111111111',
+    prefecture_id: '36',
+    city: '川崎',
+    city_number: '海域',
+    building_name: 'エンピツ'
+  )
