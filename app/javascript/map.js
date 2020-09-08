b501b3988d8daaf89b35db3d226479129710b5e2
itouma <68621609+itouma@users.noreply.github.com>
2020-09-08 17:07:44 +0900

    地図API見た目だけ導入

---
+++ app/javascript/map.js
@@ -0,0 +1,9 @@
+function initMap(){
+  let map = new google.maps.Map(document.getElementById('map'), {
+  center: {lat: -34.397, lng: 150.644},
+  zoom: 8
+  });
+}
+window.onload = function () {
+  initMap();
+}
