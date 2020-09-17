9b89f6277fb7c427c468da38804d72a25a2bde5f
itouma <68621609+itouma@users.noreply.github.com>
2020-09-17 23:48:46 +0900

    微調整,exhibitionへのリンク(仮)置き

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :exhibitions, controllers: {sessions: 'exhibitions/sessions', passwords: 'exhibitions/passwords', registrations: 'exhibitions/registrations' }
+  devise_for :exhibitions, controllers: {sessions: 'exhibitions/sessions', passwords: 'exhibitions/passwords', registrations: 'exhibitions/registrations',
+   }
+  resources :exhibitions, only: :index
 
   devise_for :customers, controllers: {sessions: 'customers/sessions', passwords: 'customers/passwords', registrations: 'customers/registrations' }
   
