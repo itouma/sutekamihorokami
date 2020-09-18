f00f5d3159a6db86cbe7e783eda2950fa5fde548
itouma <68621609+itouma@users.noreply.github.com>
2020-09-18 17:58:55 +0900

    itemの呼び出し、リンク先飛べるか確認

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,26 @@
 class PurchasesController < ApplicationController
-
+  before_action :set_item, only: [:index, :show, :destroy, :edit, :update]
   def index
-    
+    @detail =Purchase.new
+    # binding.pry
+  end
+
+  # def new
+  #   @exhibition =Purchase.new
+  # end
+
+  def create
+    @detail =Purchase.create(detail_params)
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  
+  def detail_params
+    params.require(:purchase).permit(:item, :customer)
   end
 end
