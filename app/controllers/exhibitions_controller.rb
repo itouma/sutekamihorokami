f9ec89a6cc4269f2563ab2b94d033a910f3cf6cc
itouma <68621609+itouma@users.noreply.github.com>
2020-09-27 12:23:22 +0900

    出品者ごとのページ作成、出品者の@items呼び出し

--- app/controllers/exhibitions_controller.rb
+++ app/controllers/exhibitions_controller.rb
@@ -1,3 +1,20 @@
 class ExhibitionsController < ApplicationController
-  
+  before_action :set_exhibitions, only: :show
+
+  def show
+    # @items = Item.order('created_at DESC').includes(:exhibition)
+    @items = @exhibition.items
+    # current_user.nickname
+    # @tweets = current_user.tweets
+  end
+
+  # @exhibition 
+
+
+  private
+
+  def set_exhibitions
+    @exhibition =Exhibition.find(params[:id])
+  end
+
 end
