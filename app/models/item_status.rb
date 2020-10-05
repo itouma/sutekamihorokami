bb62289ae39c3301c7568d3e01931c231ea00809
itouma <68621609+itouma@users.noreply.github.com>
2020-10-05 17:30:09 +0900

    企業top画面に取引総重量、アクティブハッシュ修正、view修正

--- app/models/item_status.rb
+++ app/models/item_status.rb
@@ -1,15 +1,10 @@
 class ItemStatus < ActiveHash::Base
   self.data = [
     { id: 0, name: '--' },
-    { id: 1, name: '経済' },
-    { id: 2, name: '政治' },
-    { id: 3, name: '地域' },
-    { id: 4, name: '国際' },
-    { id: 5, name: 'IT' },
-    { id: 6, name: 'エンタメ' },
-    { id: 7, name: 'スポーツ' },
-    { id: 8, name: 'グルメ' },
-    { id: 9, name: 'グルメ' },
-    { id: 10, name: 'その他' }
+    { id: 1, name: '賞味期限2週以内' },
+    { id: 2, name: '賞味期限1週以内' },
+    { id: 3, name: '賞味期限間近' },
+    { id: 4, name: '商品袋破損' },
+    { id: 5, name: '商品袋汚損' },
   ]
 end
